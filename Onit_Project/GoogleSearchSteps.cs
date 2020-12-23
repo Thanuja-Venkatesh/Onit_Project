249baef9dde24d4d33cc23cfc585c6dd716eee4d
THANUJA VENKATESH <thanujaselvi@gmail.com>
2020-12-23 21:24:52 +1300

    commiting

---
+++ Onit_Project/GoogleSearchSteps.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Onit_Project
+{
+    [Binding]
+    public class GoogleSearchSteps
+    {
+        private IWebDriver driver;
+
+        [Given(@"Open Google Chrome")]
+        public void GivenOpenGoogleChrome()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com/");
+            driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        [Then(@"Enter Software Tester in search bar")]
+        public void ThenEnterSoftwareTesterInSearchBar()
+        {
+            IWebElement searchtext = driver.FindElement(By.XPath("//input[@class='gLFyf gsfi']"));
+            searchtext.SendKeys("software tester");
+
+            IWebElement searchbutton = driver.FindElement(By.XPath("(//input[@aria-label='Google Search'])[2]"));
+            searchbutton.Click();
+        }
+
+        [Then(@"Click the second page and click the second result in second page")]
+        public void ThenClickTheSecondPageAndClickTheSecondResultInSecondPage()
+        {
+            IWebElement clickpage = driver.FindElement(By.XPath("//a[@aria-label='Page 2']"));
+            clickpage.Click();
+
+            IWebElement clickresult = driver.FindElement(By.XPath("(//div[@data-async-context='query:software%20tester']//div[@class='g'])[2]"));
+            clickresult.Click();
+
+            System.Threading.Thread.Sleep(100);
+        }
+
+        [Then(@"Assert the page title")]
+        public void ThenAssertThePageTitle()
+        {
+
+            String actualtitle = driver.Title;
+
+            Assert.IsNotEmpty(actualtitle);
+        }
+
+        [Then(@"close the browser")]
+        public void ThenCloseTheBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Onit_Project/googleSearch.feature
@@ -0,0 +1,14 @@
+﻿Feature: googleSearch
+	Open Google Chrome
+	Enter the text "Software Tester" in google search bar 
+	Click the second result from the second page
+	Assert the page title
+	
+
+@googleSearch
+Scenario: Search the text in google Search engine 
+	Given Open Google Chrome 
+	Then Enter Software Tester in search bar
+	Then Click the second page and click the second result in second page
+	Then Assert the page title
+	Then close the browser
